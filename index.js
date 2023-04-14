1e346ab0cffbcd89b6d4d8e8c586ac27fb33a7c1
r9xz <alynfloryn61@gmail.com>
2023-04-14 21:29:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
-
 const connectedSteamIDs = new Set();
+
 wss.on('connection', function connection(ws) {
   console.log('New client connected');
+
   ws.on('message', function incoming(message) {
     console.log('Received message:', message);
     connectedSteamIDs.add(message);
-  });
 
+    // Broadcast the incoming message to all connected clients
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
 
   ws.on('close', function close() {
     console.log('Client disconnected');
@@ -17,5 +24,3 @@ wss.on('connection', function connection(ws) {
 
   ws.send('connection successful');
 });
-
-console.log('WebSocket server running at ws://localhost:8080/');
